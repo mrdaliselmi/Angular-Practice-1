33b9d366e49e2b23897cd0efb7c4adf555edb191
MohamedAliSelmi <ma.selmi@cynoia.com>
2023-11-10 15:34:23 +0100

    🎉(Exercice 2): Add rainbow-writer component.

---
+++ src/app/rainbow-style-writer/rainbow-style-writer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-rainbow-style-writer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './rainbow-style-writer.component.html',
+  styleUrl: './rainbow-style-writer.component.css'
+})
+export class RainbowStyleWriterComponent {
+
+}
